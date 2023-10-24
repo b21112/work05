e25da39319489cc024856bcc30ab2677f852678b
b21112 <e1b21112@oit.ac.jp>
2023-10-24 14:01:25 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/apple/work05/Work05Application.java
@@ -0,0 +1,13 @@
+package oit.is.apple.work05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work05Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/apple/work05/Work05ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.apple.work05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
