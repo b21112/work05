967ff784dd1d38c129ac566c279cb5d12274d076
e1b21074 <e1b21074@oit.ac.jp>
2023-10-24 15:58:43 +0900

    購入処理

--- src/main/java/oit/is/apple/work05/model/FruitMapper.java
+++ src/main/java/oit/is/apple/work05/model/FruitMapper.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Delete;
 
 @Mapper
 public interface FruitMapper {
     @Select("select * from fruit")
     ArrayList<Fruit> selectAllFruits();
+
+    @Select("select * from fruit where id = #{id}")
+    Fruit selectById(int id);
+
+    @Delete("delete from fruit where id = #{id}")
+    boolean deleteFruitById(int id);
 }
